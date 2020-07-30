9adf27e4813d15092f433ec31bfa7cde4bf29e72
jp-caldeira <jpcm86@gmail.com>
2020-07-30 10:04:08 -0300

    teste

--- editProduto.php
+++ editProduto.php
@@ -2,16 +2,21 @@
 
 $arrayProdutos = file_get_contents('produtos.json');
 $arrayProdutos = json_decode($arrayProdutos, true);
-
 $produtoId = $_GET['produto'];
 
 
  ?>
 
+
+  
+</body>
+</html>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Produto</title>
   </head>
   <body>
